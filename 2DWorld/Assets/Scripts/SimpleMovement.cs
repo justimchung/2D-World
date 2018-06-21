9af0d89e7199e674d356824cd18b1aeea7b7c4d4
鍾毓驥 <justim@gmail.com>
2018-06-21 15:54:05 +0800

    加入讓 monster 移動的程式碼
    讓 monster 可以移動
    移動時可以改變面朝的方向

---
+++ 2DWorld/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+    public float speed;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        setStartingDirection();
+	}
+
+    private void setStartingDirection()
+    {
+        sr.flipX = (speed > 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        Vector2 temp = rb.velocity;
+        temp.x = speed;
+        rb.velocity = temp;
+    }
+}
